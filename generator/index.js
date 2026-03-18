e11ec88bd269b9547d22aefa3043b1451e94fc1d
Elsingit <elsingit@github.com>
2026-03-18 11:07:04 +0200

    created a loop function for random emoji generation

--- generator/index.js
+++ generator/index.js
@@ -2,7 +2,15 @@ const amqp = require('amqplib');
 
 const RABBIT_URL = 'amqp://rabbitmq';
 const EXCHANGE = 'name_your_channel';
-const EMOTES = ['🔥', '❤️', '😂', '👍', '🎉'];
+const EMOTES = ['🔥', '❤️', '😂', '👍', '🎉', '😭'];
+
+function randomEmote() {
+    return EMOTES[Math.floor(Math.random() * EMOTES.length)];
+}
+
+function randomBetween(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 async function start() {
     try {
@@ -16,21 +24,31 @@ async function start() {
 
         console.log(`[Generator] Connected. Publishing to "${EXCHANGE}"...`);
 
-        // TODO: This logic in now quite simplistic. 
-        // To simulate the presented scenrario, we should have times of just few
-        // random emotes and then times of many same emotes in brief window of time.
-        // Add the needed extra logic here, but don't spend too much time on it.
-        // This is just a simulation tool, after all.
-        setInterval(() => {
-            const emote = EMOTES[Math.floor(Math.random() * EMOTES.length)];
-            const msg = JSON.stringify({ emote, timestamp: Date.now() });
+        async function loop() {
 
-            // Publish to exchange. Routing key is empty string for fanout.
-            channel.publish(EXCHANGE, '', Buffer.from(msg));
-            console.log(`[Generator] Sent: ${emote}`);
-        }, 500);
-        // 2 messages per second. 
-        // A more random value would make the simulation more realistic.
+            const roll = Math.random();
+            //Burstin generointi
+            if (roll < 0.20) {
+                const burstEmote = randomEmote();
+                let emojiCount = randomBetween(10, 40);
+                for (let i = 0; i < emojiCount; i++) {
+                    console.log(`[Generator] Sent: ${burstEmote}`);
+                    await sleep(randomBetween(50, 200));
+                }
+            //Normaalin emojireagoinnin generointi
+            } else {
+                const emoji = randomEmote();
+                let emojiCount = Math.random * 4;
+                for (let i = 0; i < emojiCount; i++) {
+                    console.log(`[Generator] Sent: ${emoji}`);
+                    await sleep(randomBetween(400, 800));
+                }
+            }
+
+            await sleep(randomBetween(1000, 4000));
+            loop();
+        }  
+        loop(); 
 
     } catch (err) {
         console.error("Generator Error:", err);
