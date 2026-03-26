bd78f7dde6cff36925eb3a11523017f2e33ffef9
Juho <juho.ipi.fi>
2026-03-26 13:22:57 +0200

    add retry incase rabbit is down

--- server-a/consume.js
+++ server-a/consume.js
@@ -5,16 +5,22 @@ const QUEUE = "meaningful_moments";
 const RABBIT_URL = process.env.RABBITMQ_URL
 
 export async function startConsumer(onMessage) {
-  const conn = await amqp.connect(RABBIT_URL);
-  const channel = await conn.createChannel();
-
-  await channel.assertQueue(QUEUE, { durable: true });
-
-  channel.consume(QUEUE, (msg) => {
-    if (msg !== null) {
-      const data = JSON.parse(msg.content.toString());
-      onMessage(data);
-      channel.ack(msg);
+  while (true) {
+    try {
+      const conn = await amqp.connect(RABBIT_URL);
+      const channel = await conn.createChannel();
+      await channel.assertQueue(QUEUE, { durable: true });
+      channel.consume(QUEUE, (msg) => {
+        if (msg !== null) {
+          const data = JSON.parse(msg.content.toString());
+          onMessage(data);
+          channel.ack(msg);
+        }
+      });
+      break;
+    } catch (err) {
+      console.log("Connection failed, retry in 5s", err.message);
+      await new Promise((r) => setTimeout(r, 5000));
     }
-  });
+  }
 }
